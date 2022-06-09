d4164a1458708b119a741b7bd54ff7c4f6f58aea
rishadpt <rishadpt16@gmail.com>
2022-06-09 09:59:31 +0530

    fix : alignment fixed

--- src/components/Table/Pagination/Pagination.tsx
+++ src/components/Table/Pagination/Pagination.tsx
@@ -3,13 +3,13 @@ import { paginationProps } from '../../../utils/types';
 import './Pagination.scss'
 
 export default function Pagination({ totalPages, postsPerPage, activePage, currentpage }: paginationProps) {
-  const [pagenolimit,setPagenolimit] = useState<number>(5)
-  const [minimumpagelimit,setMinimumpagelimit] = useState<number>(0)
-  const [maximumpagelimit,setMaximumpagelimit] = useState<number>(5)
+  const [pagenolimit, setPagenolimit] = useState<number>(5)
+  const [minimumpagelimit, setMinimumpagelimit] = useState<number>(0)
+  const [maximumpagelimit, setMaximumpagelimit] = useState<number>(5)
 
   const pageNumbers = [];
-  console.log("total",totalPages)
-  for (let i =1 ; i <= Math.ceil(totalPages / postsPerPage); i++) {
+  console.log("total", totalPages)
+  for (let i = 1; i <= Math.ceil(totalPages / postsPerPage); i++) {
     pageNumbers.push(i);
 
   }
@@ -19,35 +19,35 @@ export default function Pagination({ totalPages, postsPerPage, activePage, curre
     return new Array(pagenolimit).fill(undefined).map((_, idx) => start + idx + 1);
   };
 
-  function changePage(e:any) {
+  function changePage(e: any) {
     const pageNumber = Number(e.target.textContent);
     console.log(pageNumber)
-   activePage(pageNumber);
+    activePage(pageNumber);
   }
   function goToNextPage() {
     console.log(totalPages)
-    if (currentpage< totalPages/postsPerPage  ) {
+    if (currentpage < totalPages / postsPerPage) {
       activePage(currentpage + 1);
     }
   }
   function goTobackPage() {
-    if(currentpage > 1){
-    activePage( currentpage - 1);
+    if (currentpage > 1) {
+      activePage(currentpage - 1);
     }
   }
 
   console.log(postsPerPage)
   return (
-    
-    <ul className="pagination">
-        <p onClick={goTobackPage}>Previous Page</p>
-       {getPaginationGroup().map((number: number) => (
-          <li 
-          style={{display:`${totalPages/postsPerPage+1 <= number && 'none' }`}} className={(currentpage === number) ? 'page-item selected' : ''} onClick={(e) =>{ changePage(e)}} key={number} >
-          { number}
+
+    <ul style={{marginLeft: `${currentpage === 1 ? '2rem' : '0'}`}} className="pagination">
+      <p style={{ display: `${currentpage === 1 ? 'none' : 'block'}` }} onClick={goTobackPage}>Previous Page</p>
+      {getPaginationGroup().map((number: number) => (
+        <li
+          style={{ display: `${totalPages / postsPerPage + 1 <= number && 'none'}` }} className={(currentpage === number) ? 'page-item selected' : ''} onClick={(e) => { changePage(e) }} key={number} >
+          {number}
         </li>
       ))}
-      <p style={{display:`${currentpage === totalPages/postsPerPage ? 'none' : 'block' }`}} onClick={goToNextPage}>NEXT PAGE</p>
+      <p style={{ display: `${currentpage === totalPages / postsPerPage ? 'none' : 'block'}` }} onClick={goToNextPage}>NEXT PAGE</p>
     </ul>
   )
 
